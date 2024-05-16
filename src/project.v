5eedddc2bb6e342b99e26396ea40dfb4039c443c
Mahaa Santeep G <82812064+Santeep@users.noreply.github.com>
2024-05-16 20:33:38 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -10,7 +10,7 @@ module tt_um_serdes (
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
-    //output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
@@ -36,5 +36,5 @@ module tt_um_serdes (
         .data_out(uo_out),
         .data_8b_in(ui_in)  
     );
-        
+      assign uio_oe = 8'b0;  
 endmodule
